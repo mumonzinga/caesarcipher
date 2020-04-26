49b4652d565320eb90b737423f57f2515d45a01e
Mumo <mnzinga888@gmail.com>
2020-04-26 19:52:04 +0300

    final app.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -133,10 +133,11 @@ public class App {
         } else  if (request.equalsIgnoreCase("quit")) {
             System.out.println("Ending the application ... ");
             System.exit(0);
-        } else  if (request.equalsIgnoreCase("quit")) {
-            System.out.println("Ending the application ... ");
-            System.exit(0);
+        } else {
+            System.out.println("Invalid request, please choose encrypt or decrypt");
 
+
+        }
         }
     }
 }
