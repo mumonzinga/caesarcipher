5957236e5fe7f785382eac7ee7ed89ab9216bd92
Mumo <mnzinga888@gmail.com>
2020-04-26 12:59:33 +0300

    add encrypt

---
+++ src/main/java/CaeserCipher.java
@@ -0,0 +1,94 @@
+public class CaeserCipher {
+    private String encodedRightText;
+    private String encodedLeftText;
+    private String decodedText;
+    private int latchKey;
+    private int approvedKey;
+
+    public CaeserCipher(String encryptedRightText, String encryptedLeftText, String decryptedText, int shiftKey) {
+        this.encodedRightText = encryptedRightText;
+        this.encodedLeftText = encryptedLeftText;
+        this.decodedText = decryptedText;
+        this.latchKey = shiftKey;
+        this.approvedKey = chosenKey;
+    }
+
+        public String getEncodedRightText() {
+            return encodedRightText;
+        }
+        public String getEncodedLeftText() {
+        return encodedLeftText;
+        }
+        public String getDecodedText() {
+        return decodedText;
+        }
+
+        public int getLatchKey() {
+        return latchKey;
+        }
+        public int getApprovedKey() {
+        return approvedKey;
+        }
+
+    public static final String alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+    public static String encryptRight(String encodedRightText, int latchKey) {
+        encodedRightText = encodedRightText.toLowerCase();
+        char[] cr = encodedRightText.toCharArray();
+        String cipherRightText = " ";
+
+
+        for (int i = 0; i < encodedRightText.length(); i++) {
+
+
+            int charRightKey = alphabet.indexOf(encodedRightText.charAt(i));
+            int indexRight = (latchKey + charRightKey) % 26;
+            char CipherRightch = alphabet.charAt(indexRight);
+            cipherRightText = String.valueOf(CipherRightch);
+
+        }
+
+
+        return cipherRightText;
+    }
+    public static String encryptLeft(String encodedLeftText, int latchKey) {
+        encodedLeftText = encodedLeftText.toLowerCase();
+        char[] cl = encodedLeftText.toCharArray();
+        String cipherLeftText = "";
+
+        for (int i = 0; i < encodedLeftText.length(); i--) {
+
+            int charLeftKey = alphabet.indexOf(encodedLeftText.charAt(i));
+            int indexLeft = (latchKey - charLeftKey) % 26;
+            char CipherLeftch = alphabet.charAt((indexLeft));
+            cipherLeftText = String.valueOf(CipherLeftch);
+
+        }
+        return cipherLeftText;
+    }
+    public static String Decrypt(String decodedText,int approvedKey) {
+        decodedText = decodedText.toLowerCase();
+        char[] cd = decodedText.toCharArray();
+        boolean approvedKeyRight = 1>26;
+        String decodeRight;
+        String error = "";
+        String cipherDecode = "";
+
+        if (approvedKey <1 || approvedKey > 26) {
+            error = "key must be between 1 to 25";
+        } else if (approvedKey == -(approvedKey) {
+            for  (int i = 0; i < cipherDecode.length(); i++)
+            {
+                int charDeKey = alphabet.indexOf(cipherDecode.charAt(i));
+                int chosenKey = (charDeKey - approvedKey) % 26;
+                if (chosenKey <  0)
+                {
+                    chosenKey = alphabet.length() + chosenKey;
+                }
+                char cipherDecodech = alphabet.charAt(chosenKey);
+                cipherDecode = String.valueOf(cipherDecodech);
+            }
+            return cipherDecode;
+        }
+    }
+}
